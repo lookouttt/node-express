c7aa8df0433bdfab1fb1735f8f85f01b2fb79c47
lookouttt <lookoutt@hotmail.com>
2023-02-17 12:55:26 -0800

    Cleanup after video review

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ app.use(morgan('dev'));
 app.use(express.static(__dirname + '/public'));
 
 app.use((req, res) => {
-    console.log(req.headers);
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<html><body><h1>This is an Express Server</h1></body></html>');
